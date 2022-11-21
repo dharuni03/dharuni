058490fbb9f0d120203b4d96140f0697fa44405c
Dharuni K <727721euit030@skcet.ac.in>
2022-11-21 15:59:08 +0530

    first commit

---
+++ src/signin.js
@@ -0,0 +1,36 @@
+import "./signin.css";
+const App = () => {
+  return (
+    <div className="mac-book-pro-16-8">
+      <div className="app-16">
+        <div className="akar-iconsarrow-left"></div>
+        <span classN
+        ame="create-your-account">Create your account</span>
+        <div className="flex-container">
+          <input className="rectangle-1" placeholder="Email" type="text" />
+          <span className="or-continue-with">or continue with</span>
+        </div> 
+        <div className="flex-container-1">
+          <div className="rectangle-2">
+            <div className="teenyiconspassword-s"></div>
+            <span className="password">Password</span>
+          </div>
+          <div className="rectangle-4"></div>
+          <div className="rectangle-5"></div>
+          <div className="rectangle-6"></div>
+        </div>
+        <div className="rectangle-3">
+          <span className="sign-up"> Sign up</span>
+        </div>
+        <div className="flex-container-2">
+          <span>Already have an account? </span>
+          <span className="sign-in">Sign in</span>
+        </div>
+        </div>
+        </div>
+
+
+  
+  );
+};
+export default App;
